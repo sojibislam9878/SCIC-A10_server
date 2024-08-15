69dcc0587ae94723264b1e89d05fa935c80eada6
sojibhasan9878 <sojibhasan9878@gmail.com>
2024-08-15 23:15:53 +0600

    some changes for vercel deployment

--- index.js
+++ index.js
@@ -27,10 +27,6 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-      // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
-      // Send a ping to confirm a successful connection
-      await client.db("admin").command({ ping: 1 });
     const itemCollection = client.db("scicItemsDB").collection("allItems")
     const usersCollection = client.db("scicItemsDB").collection("allUsers")
 
@@ -151,10 +147,6 @@ async function run() {
         const result =await usersCollection.updateOne(query, updateDocs, options)
         res.send(result)
       })
-
-
-
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
