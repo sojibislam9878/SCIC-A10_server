281d9690f213c9400e523e79cc8264b06a421c24
Sojib Islam <135939235+sojibislam9878@users.noreply.github.com>
2025-03-15 10:45:38 +0600

    api is public

--- index.js
+++ index.js
@@ -6,13 +6,22 @@ const app = express()
 const port = process.env.PORT || 3000
 
 // middleware
-app.use(express.json());
+
+// app.use(express.json());
+// const corsOptions = {
+//     origin: ['http://localhost:5173', 'http://localhost:5174',"https://scic-a10.web.app"],
+//     credentials: true,
+//     optionSuccessStatus: 200,
+//   }
+//   app.use(cors(corsOptions))
+
 const corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:5174',"https://scic-a10.web.app"],
-    credentials: true,
+    origin: '*',
+    credentials: false,
     optionSuccessStatus: 200,
-  }
-  app.use(cors(corsOptions))
+};
+
+app.use(cors(corsOptions));
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lb51cqq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
